e343f58047f08ef83b8a93927d8d2df8eda24388
Mohamed Mohsen <moha476476@gmail.com>
2026-02-22 16:24:06 +0200

    edit generator again

--- dataGenerateor.js
+++ dataGenerateor.js
@@ -3,9 +3,9 @@ import { formatData } from "./dataFormat.js";
 export function* generator() {
   if (!formatData) {
     return "no data";
-  }
-
-  for (const user of formatData) {
-    yield user;
+  } else {
+    for (const user of formatData) {
+      yield user;
+    }
   }
 }
